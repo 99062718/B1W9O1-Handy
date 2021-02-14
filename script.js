92d59404bdcd3192686ff6d459d9f4d6d7b60856
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2021-02-14 15:49:25 +0100

    update

--- script.js
+++ script.js
@@ -28,9 +28,21 @@ function fadeBack3(){
 	document.getElementById("img3").style.opacity = "1";
 }
 
+function buttonHover(){
+	document.getElementById("button").style.backgroundColor = "gray";
+	document.getElementById("button").style.color = "white";
+}
+
+function buttonNotHover(){
+	document.getElementById("button").style.backgroundColor = "white";
+	document.getElementById("button").style.color = "black";
+}
+
 document.getElementById("box1").addEventListener("mouseover", fadeAway1);
 document.getElementById("box1").addEventListener("mouseout", fadeBack1);
 document.getElementById("box2").addEventListener("mouseover", fadeAway2);
 document.getElementById("box2").addEventListener("mouseout", fadeBack2);
 document.getElementById("box3").addEventListener("mouseover", fadeAway3);
 document.getElementById("box3").addEventListener("mouseout", fadeBack3);
+document.getElementById("button").addEventListener("mouseover", buttonHover);
+document.getElementById("button").addEventListener("mouseout", buttonNotHover);
